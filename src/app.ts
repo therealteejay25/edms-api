95842bcd6e9fca9d2a64f7509a9ef801660ab03b
therealteejay25 <therealteejay25@gmail.com>
2025-11-26 20:32:35 +0100

    i

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-const FRONTEND = process.env.FRONTEND_URL || "http://localhost:3000";
+const FRONTEND = "https://zentra-kappa.vercel.app";
 app.use(
   cors({
     origin: FRONTEND,
